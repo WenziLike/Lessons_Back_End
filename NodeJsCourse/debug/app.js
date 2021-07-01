e6591601f08c0ba4fe9edbfd8196691ff79c3e78
WenziLike <SlavaMyraxin@icloud.com>
2021-07-01 23:05:57 +0200

    add debug

---
+++ NodeJsCourse/debug/app.js
@@ -0,0 +1,18 @@
+let a = 6
+let b = 3;
+// b = b * 2
+// a = a + b
+// console.log(a)
+// console.log(b)
+// console.log(a + b)
+
+
+function t() {
+    let a = 8
+    let c = a + b
+    a = a + d
+    console.log(a + c)
+}
+
+// t()
+
