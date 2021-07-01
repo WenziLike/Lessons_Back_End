4c80ab44cd46f7547846b2455c583e89d3607772
WenziLike <SlavaMyraxin@icloud.com>
2021-07-01 23:55:51 +0200

    Ajax GET POST JS

---
+++ NodeJsCourse/Ajax_GET_POSTrequest/app.js
@@ -0,0 +1,34 @@
+/**
+ * GET -> получить обработать
+ */
+
+const http = require('http')
+const url = require('url')
+const {parse} = require('querystring')
+
+http.createServer((request, response) => {
+    console.log(`STARTED SERVER`)
+    if (request.method === 'GET') {
+        console.log(request.method)
+        let urlRequest = url.parse(request.url, true)
+        // console.log(urlRequest)
+        console.log(urlRequest.query.test) // ! GET Params
+        response.end('go go go')
+    } else {
+        /**
+         * POST -> получить обработать
+         */
+
+        let body = ''
+        request.on('data', chunk => {
+            body += chunk.toString()
+        })
+        request.on('end', () => {
+            console.log(body)
+            let params = parse(body)
+            console.log(params)
+            console.log(params.hi)
+            response.end('...ok')
+        })
+    }
+}).listen(3000)
