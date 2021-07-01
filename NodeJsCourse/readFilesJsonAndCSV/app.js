0317abd63b7a564417190d0aa3711e123a6c0212
WenziLike <SlavaMyraxin@icloud.com>
2021-07-01 22:39:36 +0200

    add writeRecords

--- NodeJsCourse/readFilesJsonAndCSV/app.js
+++ NodeJsCourse/readFilesJsonAndCSV/app.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const path = require('path')
 const student = require('./one.json')
 const csv = require('csv-parser')
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 fs.writeFile('one.txt', 'work', err => {
     if (err) console.log('Error')
@@ -43,5 +44,51 @@ const results = []
  * Case 4. Пишем CSV file
  */
 
+const csvWriter = createCsvWriter({
+    path: 'test.csv',
+    header: [
+        {id: 'name', title: 'NAME'},
+        {id: 'surname', title: 'LastName'},
+        {id: 'age', title: 'AGE'},
+        {id: 'gender', title: 'G'}
+    ]
+});
 
+const data = [
+    {
+        name: 'John',
+        surname: 'Doe',
+        age: 24,
+        gender: 'M'
+    },
+    {
+        name: 'Slava',
+        surname: 'Mur',
+        age: 32,
+        gender: 'M'
+    },
+    {
+        name: 'Ada',
+        surname: 'Jens',
+        age: 23,
+        gender: 'W'
+    },
+    {
+        name: 'Ola',
+        surname: 'Click',
+        age: 30,
+        gender: 'W'
+    },
+    {
+        name: 'Din',
+        surname: 'Don',
+        age: 45,
+        gender: 'M'
+    },
+]
+
+csvWriter.writeRecords(data)       // returns a promise
+    .then(() => {
+        console.log('...Done');
+    });
 
